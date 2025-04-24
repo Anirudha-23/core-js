5d30b9a130d8109472ca45feaf573cfb97305350
Anirudha-23 <swevintern4@chargeworks.in>
2025-04-24 14:55:23 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,21 @@
+let a = 10, b = 5;
+console.log('Arithmetic Operators:');
+console.log(a + b); // Addition: 
+console.log(a - b); // Subtraction: 
+console.log(a * b); // Multiplication: 
+console.log(a / b); // Division: 
+console.log(a % b); // Modulus: 
+console.log(a ** b); // Exponentiation: 
+console.log('Comparison Operators:');
+console.log(a == b);  // Equal to: false
+console.log(a === b); // Strict equal to: false
+console.log(a != b);  // Not equal to: true
+console.log(a !== b); // Strict not equal to: true
+console.log(a > b);   // Greater than: true
+console.log(a < b);   // Less than: false
+console.log(a >= b);  // Greater than or equal to: true
+console.log(a <= b);  // Less than or equal to: false
+console.log('Logical Operators:');
+console.log(true && false); // AND: false
+console.log(true || false); // OR: true
+console.log(!true);         // NOT: false
