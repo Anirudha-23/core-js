30d230d2ad36feb1ca929c33d12c987ab8f5f07a
Anirudha-23 <swevintern4@chargeworks.in>
2025-04-24 14:49:56 +0530

    data Type

---
+++ dataType.js
@@ -0,0 +1,7 @@
+let age = 21; // integer
+let temperature = 37.5; // float
+let emptyValue = null; // explicitly assigned as null
+let fullName = "Ani"; // string 
+let numbers = [1, 2, 3, 4, 5]; // array
+let isActive = true;  // Boolean variable
+let isLoggedIn = false; // Boolean variable
