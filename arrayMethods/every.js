8e8b9487a38eac9ed48b30f61d6e4e98aa09e38d
Anirudha-23 <swevintern4@chargeworks.in>
2025-04-24 23:20:49 +0530

    file map, reduce, filter, some, every

---
+++ arrayMethods/every.js
@@ -0,0 +1,3 @@
+const marks = [70, 85, 90];
+const allPassed = marks.every(mark => mark >= 50);
+console.log(allPassed); // true
---
+++ arrayMethods/filter.js
@@ -0,0 +1,3 @@
+const ages = [12, 18, 22, 16];
+const adults = ages.filter(age => age >= 18);
+console.log(adults); // [18, 22]
